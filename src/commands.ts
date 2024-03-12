06ba819bddecbd3d39b83fa237b404e9fea67782
Fabio Spampinato <spampinabio@gmail.com>
2024-03-12 01:48:13 +0000

    Minor internal improvements

--- src/commands.ts
+++ src/commands.ts
@@ -1,28 +1,20 @@
 
 /* IMPORT */
 
-import fs from 'node:fs';
-import path from 'node:path';
 import process from 'node:process';
-import openPath from 'tiny-open';
-import vscode from 'vscode';
-import {getProjectRootPath} from 'vscode-extras';
+import {alert, getGitRootPath, openInApp} from 'vscode-extras';
 
 /* MAIN */
 
 const open = (): void => {
 
-  const rootPath = getProjectRootPath ();
+  const rootPath = getGitRootPath ();
 
-  if ( !rootPath ) return void vscode.window.showErrorMessage ( 'You have to open a project before being able to open it in GitTower' );
-
-  const gitPath = path.join ( rootPath, '.git' );
-
-  if ( !fs.existsSync ( gitPath ) ) return void vscode.window.showErrorMessage ( 'The project needs to be a git repository for opening it in GitTower' );
+  if ( !rootPath ) return alert.error ( 'You have to open a git repository before being able to open it in GitTower' );
 
   const app = ( process.platform === 'darwin' ) ? 'Tower' : 'tower';
 
-  openPath ( rootPath, { app } );
+  openInApp ( rootPath, app );
 
 };
 
